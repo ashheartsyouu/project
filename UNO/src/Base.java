607ded9399601fe42f9aad6f4ce05555bf76dde9
Taylor Holmes <tsh92872@uga.edu>
2018-04-13 14:05:27 -0400

    Update Base.java

--- UNO/src/Base.java
+++ UNO/src/Base.java
@@ -1,39 +1,46 @@
 import java.util.*;
 public class Base {
+	public ArrayList<Players> players = new ArrayList<Players>();
 	private Scanner scnr = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-			// TODO Auto-generated method stub
+		
 		Scanner scnr = new Scanner(System.in);
-		System.out.println("Welcome to UNO!");
-		System.out.println("Please enter Player 1's name: ");
-		String p1 = scnr.next();
-		System.out.println("Please enter Player 2's name: ");
-		String p2 = scnr.next();
+		Players thePlayer = new Players();
+		
+		System.out.println("Welcome to UNO!");		
+		System.out.println("Would you like to begin? Y or N");
+		String response = scnr.next();
+		
+		thePlayer.addComputer();	//Generates computer player as players.get(0) --> index = 0
+		
+		if (response.toUpperCase().equals("Y")) {//Allows for players to be created and added to the players ArrayList
+			thePlayer.isThereAPlayer();	//This function will call the add player function, if "y"
+		}
+		else {
+			System.out.println("Goodbye");
+		}
+		
+		thePlayer.printPlayers();	//Shows all players, including computer.
 		
 		
 		
 		Deck deck = new Deck();
-		Players player1 = new Players(p1);
-
-		
-		Players player2 = new Players(p2);
 		
 		
-		
-		for(int i = 0; i < 7; ++i) {
-			player1.takeCard(deck.gimmeACard());
+// 		for(int i = 0; i < 7; ++i) {
+// 			player1.takeCard(deck.gimmeACard());
 			
-			player2.takeCard(deck.gimmeACard());
-		}
+// 			player2.takeCard(deck.gimmeACard());
+// 		}
 				
-		player1.printHand();
-		player2.printHand();
+// 		player1.printHand();
+// 		player2.printHand();
 		
-		System.out.print("First Card: ");
-		deck.gimmeACard().printInfo();
+// 		System.out.print("First Card: ");
+// 		deck.gimmeACard().printInfo();
 		
-		System.out.println(p1 + "'s turn. What would you like to do?");
+// 		System.out.println(p1 + "'s turn. What would you like to do?");
 		
 		
 	}
